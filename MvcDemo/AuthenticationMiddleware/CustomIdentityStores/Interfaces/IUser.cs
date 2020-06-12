5d339bb600f2014c0db443f5913469b9eeeec6de
mrn-aglic <mrn.aglic@gmail.com>
2020-06-12 19:59:42 +0200

    Added IUser file

---
+++ MvcDemo/AuthenticationMiddleware/CustomIdentityStores/Interfaces/IUser.cs
@@ -0,0 +1,10 @@
+namespace MvcDemo.AuthenticationMiddleware.CustomIdentityStores.Interfaces
+{
+    public interface IUser<T>
+    {
+        public T Id { get; set; }
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
